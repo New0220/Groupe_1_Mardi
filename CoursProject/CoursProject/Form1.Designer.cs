70852e87224629230dc824aa54bb91d9a8afba01
Kameni <kamenifrancler@gmail.com>
2022-04-24 18:32:18 -0400

    Salut a tous voici la version ameliorer de ma classe cours.

---
+++ CoursProject/CoursProject/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CoursProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.listViewCours = new System.Windows.Forms.ListView();
+            this.NumeroDuCours = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Code = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Titre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.Modifier = new System.Windows.Forms.Button();
+            this.Enregistrer = new System.Windows.Forms.Button();
+            this.Supprimer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(69, 60);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(107, 16);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "NumeroDuCours";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(69, 109);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(40, 16);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "Code";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(69, 157);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(34, 16);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Titre";
+            // 
+            // listViewCours
+            // 
+            this.listViewCours.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NumeroDuCours,
+            this.Code,
+            this.Titre});
+            this.listViewCours.FullRowSelect = true;
+            this.listViewCours.GridLines = true;
+            this.listViewCours.HideSelection = false;
+            this.listViewCours.Location = new System.Drawing.Point(331, 57);
+            this.listViewCours.Name = "listViewCours";
+            this.listViewCours.Size = new System.Drawing.Size(516, 253);
+            this.listViewCours.TabIndex = 3;
+            this.listViewCours.UseCompatibleStateImageBehavior = false;
+            this.listViewCours.View = System.Windows.Forms.View.Details;
+            // 
+            // NumeroDuCours
+            // 
+            this.NumeroDuCours.Text = "NumeroDuCours";
+            this.NumeroDuCours.Width = 200;
+            // 
+            // Code
+            // 
+            this.Code.Text = "Code";
+            this.Code.Width = 100;
+            // 
+            // Titre
+            // 
+            this.Titre.Text = "Titre";
+            this.Titre.Width = 100;
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.Location = new System.Drawing.Point(101, 383);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.Ajouter.TabIndex = 4;
+            this.Ajouter.Text = "Ajouter";
+            this.Ajouter.UseVisualStyleBackColor = true;
+            this.Ajouter.Click += new System.EventHandler(this.Ajouter_Click);
+            // 
+            // Modifier
+            // 
+            this.Modifier.Location = new System.Drawing.Point(275, 384);
+            this.Modifier.Name = "Modifier";
+            this.Modifier.Size = new System.Drawing.Size(75, 23);
+            this.Modifier.TabIndex = 5;
+            this.Modifier.Text = "Modifier";
+            this.Modifier.UseVisualStyleBackColor = true;
+            this.Modifier.Click += new System.EventHandler(this.Modifier_Click);
+            // 
+            // Enregistrer
+            // 
+            this.Enregistrer.Location = new System.Drawing.Point(458, 384);
+            this.Enregistrer.Name = "Enregistrer";
+            this.Enregistrer.Size = new System.Drawing.Size(93, 23);
+            this.Enregistrer.TabIndex = 6;
+            this.Enregistrer.Text = "Enregistrer";
+            this.Enregistrer.UseVisualStyleBackColor = true;
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.Location = new System.Drawing.Point(612, 383);
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Size = new System.Drawing.Size(89, 23);
+            this.Supprimer.TabIndex = 7;
+            this.Supprimer.Text = "Supprimer";
+            this.Supprimer.UseVisualStyleBackColor = true;
+            this.Supprimer.Click += new System.EventHandler(this.Supprimer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 526);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Supprimer);
+            this.Controls.Add(this.Enregistrer);
+            this.Controls.Add(this.Modifier);
+            this.Controls.Add(this.Ajouter);
+            this.Controls.Add(this.listViewCours);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.ListView listViewCours;
+        private System.Windows.Forms.Button Ajouter;
+        private System.Windows.Forms.Button Modifier;
+        private System.Windows.Forms.Button Enregistrer;
+        private System.Windows.Forms.Button Supprimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ColumnHeader NumeroDuCours;
+        private System.Windows.Forms.ColumnHeader Code;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.ColumnHeader Titre;
+    }
+}
+
---
+++ CoursProject/CoursProject/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CoursProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            string[] tInfos = new string[] { textBox1.Text, textBox2.Text, textBox3.Text };
+            ListViewItem list = new ListViewItem(tInfos);
+
+            listViewCours.Items.Add(list);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void Supprimer_Click(object sender, EventArgs e)
+        {
+            if (listViewCours.SelectedItems.Count > 0)
+            {
+                listViewCours.Items.Remove(listViewCours.SelectedItems[0]);
+            }
+        }
+
+       private void Enregistrer_Click(object sender, EventArgs e)
+        {
+            StreamWriter saveList = new StreamWriter ("C:/Users/kamen/source/repos/CoursProject/CoursProject/Cour.txt", false);
+            foreach (ListViewItem item in listViewCours.SelectedItems)
+            {
+                saveList.WriteLine(item.SubItems[0].Text + "/" +item.SubItems[1].Text +"/"+ item.SubItems[2].Text);
+            }
+            saveList.Close();
+        }
+
+        int indiceitem;
+        private void Modifier_Click(object sender, EventArgs e)
+        {
+            if (listViewCours.SelectedItems.Count > 0)
+            {
+                 indiceitem = listViewCours.SelectedIndices[0];
+                textBox1.Text = listViewCours.Items[indiceitem].SubItems[0].Text;
+                textBox2.Text = listViewCours.Items[indiceitem].SubItems[1].Text;
+                textBox3.Text = listViewCours.Items[indiceitem].SubItems[2].Text;
+                
+            }
+            modif();
+        }
+        private void modif()
+        {
+            listViewCours.Items[indiceitem].SubItems[0].Text = textBox1.Text;
+            listViewCours.Items[indiceitem].SubItems[1].Text = textBox2.Text;
+            listViewCours.Items[indiceitem].SubItems[2].Text = textBox3.Text;
+        }
+    }
+}
